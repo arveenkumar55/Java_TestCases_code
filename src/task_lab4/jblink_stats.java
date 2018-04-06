328bfe7d5dd22b8c7bccd27a940cef06c843fa03
Mohamed Jubara Jubara <000322930@mohawkcollege.ca>
2018-04-06 09:38:44 +0500

    "COMMIT#2 - Source Code Improvements"

--- src/task_lab4/jblink_stats.java
+++ src/task_lab4/jblink_stats.java
@@ -1,114 +1,156 @@
 package task_lab4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Library of statistical functions using Generics for different statistical
  * calculations.
  *
- * @author Joey Programmer
+ * @author Mohamed Jubara Jubara 
  */
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public final class jblink_stats {
+    
+    
+    /**
+ * @param numbersList first Generics type of list Numbers
+ * @return Average  numbersList numbers
+ 
+ */
 
-    public static <T extends Number> double avg(ArrayList<T> x, boolean incneg) {
-        double s = sum(x, incneg);
-        int c = 0;
-        for (int i = 0; i < x.size(); i++) {
-            if (incneg || x.get(i).doubleValue() >= 0) {
-                c++;
+    public static <T extends Number> double computeAverage(ArrayList<T> numbersList) {
+        double totalSum = computeSum(numbersList);
+        int countNumbers = 0;
+        for (int i = 0; i < numbersList.size(); i++) {
+            if ( numbersList.get(i).doubleValue() >= 0) {
+                countNumbers++;
             }
         }
-        if (c == 0) {
-            throw new IllegalArgumentException("no values are > 0");
+        if (countNumbers == 0) {
+            //throw new IllegalArgumentException("no values are > 0");
+            return -1;
         }
-        return s / c;
+        return totalSum/countNumbers;
     }
 
-    public static <T extends Number> double sum(ArrayList<T> x, boolean incneg) {
-        if (x.size() < 0) {
-            throw new IllegalArgumentException("x cannot be empty");
+      /**
+ * @param numbersList first Generics type of list Numbers
+ * @return Sum  numbersList numbers
+ 
+ */ 
+    
+    public static <T extends Number> double computeSum(ArrayList<T> numbersList) {
+        if (numbersList.size() < 1) {
+            //throw new IllegalArgumentException("numbersList cannot be empty");
+            return -1;
         }
 
         double sum = 0.0;
-        for (T val : x) {
+        for (T val : numbersList) {
+            
             double value = val.doubleValue();
-            if (incneg || value >= 0) {
+            
+            if ( value >= 0) {
                 sum += value;
+                //System.out.println(sum);
             }
         }
         return sum;
     }
 
-    public static <T extends Number> double med(ArrayList<T> x) {
+    /**
+ * @param numbersList first Generics type of list Numbers
+ * @return median  numbersList numbers
+ 
+ */ 
+    
+    
+    public static <T extends Number> double computeMedian(ArrayList<T> numbersList) {
         double med;
-        if (x.size()%2==0) {
-            int m = x.size()/2-1;
-            med = (fnv(x, m) + fnv(x,m+1))/2;
+        if (numbersList.size()%2==0) {
+            int length = numbersList.size()/2-1;
+            med = (fnv(numbersList, length) + fnv(numbersList,length+1))/2;
         }
         else
-            med = fnv(x, x.size()/2);
+            med = fnv(numbersList, numbersList.size()/2);
         return med;
     }
     
-    private static <T extends Number> double fnv(ArrayList<T> x, int p) 
+    private static <T extends Number> double fnv(ArrayList<T> numbersList, int size) 
     {
         ArrayList<T> lt = new ArrayList<>();
         ArrayList<T> gt = new ArrayList<>();
         
-        int pv = x.size()/2;
+        int pv = numbersList.size()/2;
         
-        double pv_val = x.get(pv).doubleValue();
-        for (T v : x) {
+        double pv_val = numbersList.get(pv).doubleValue();
+        for (T v : numbersList) {
             if (v.doubleValue() < pv_val)
                 lt.add(v);
             else if (v.doubleValue() > pv_val)
                 gt.add(v);
         }
         
-        if(p < lt.size()) 
-            return fnv(lt, p);
+        if(size < lt.size()) 
+            return fnv(lt, size);
         
-        else if(p >= (x.size() - gt.size())) 
-            return fnv(gt,p - (x.size() - gt.size()));
+        else if(size >= (numbersList.size() - gt.size())) 
+            return fnv(gt,size - (numbersList.size() - gt.size()));
 
         return pv_val;
     }
-
-    public static <T extends Number> double sd(ArrayList<T> data) {
+   /**
+ * @param data first Generics type of list Numbers
+ * @return standard deviation  numbersList numbers
+ 
+    public static <T extends Number> double standardDeviation(ArrayList<T> data) {
         if (data.size() <= 1) {
-            throw new IllegalArgumentException("Size of array must be greater than 1");
+ */
+    public static <T extends Number> double computeStandardDeviation(ArrayList<T> data) {
+        if (data.size() <= 1) {
+           // throw new IllegalArgumentException("Size of array must be greater than 1");
+           return -1;
         }
 
-        int n = data.size();
-        double s = 0;
-        double a = avg(data,true);
+        int size = data.size();
+        double sum = 0;
+        double avarageValue = computeAverage(data);
         
-        for (int i = 0; i < n; i++) {
-            double v = data.get(i).doubleValue();
-            s += Math.pow(v - a,2);
+        for (int i = 0; i < size; i++) {
+            double value = data.get(i).doubleValue();
+            sum += Math.pow(value - avarageValue,2);
         }
-        double stdev = Math.sqrt(s / (n-1));
+        double stdev = Math.sqrt(sum / (size-1));
         return stdev;
     }
+    
 
     // Simple set of tests that confirm that functions operate correctly
-    public static void main(String[] args) {
+  /**
+ * Main function execute first when program run.
+ *
+ */
+
+   /* public static void main(String[] args) {
 
         ArrayList<Integer> testDataI = new ArrayList<>(Arrays.asList(9,2,7,4,5,6,3,8,1,10));
         ArrayList<Double> testDataD = new ArrayList<>(Arrays.asList(2.2, 100.0, 17.5, 30.2, 31.1));
 
-        System.out.printf("The sum of the Integer array = %.0f\n", sum(testDataI, true));
-        System.out.printf("The sum of the Double array = %.0f\n", sum(testDataD, true));
-
-        System.out.printf("The average of the Integer test set = %.2f\n", avg(testDataI, true));
-        System.out.printf("The average of the Double test set = %.2f\n", avg(testDataD, true));
-
-        System.out.printf("The median value of the Integer data set = %.2f\n", med(testDataI));
+      
         
-        System.out.printf("The median value of the Double data set = %.2f\n", med(testDataD));
+        System.out.printf("The sum of the Integer array = %.0f\n", sumNumbers(testDataI));
+       System.out.printf("The sum of the Double array = %.0f\n", sumNumbers(testDataD));
+
+        System.out.printf("The average of the Integer test set = %.2f\n", averageNumbers(testDataI));
+        System.out.printf("The average of the Double test set = %.2f\n", averageNumbers(testDataD));
+
+        System.out.printf("The median value of the Integer data set = %.2f\n", medianNumbers(testDataI));
         
-        System.out.printf("The sample standard deviation of the Integer test set = %.2f\n", sd(testDataI));
-        System.out.printf("The sample standard deviation of the Double test set = %.2f\n", sd(testDataD));
+        System.out.printf("The median value of the Double data set = %.2f\n", medianNumbers(testDataD));
+        
+        System.out.printf("The sample standard deviation of the Integer test set = %.2f\n", standardDeviation(testDataI));
+        System.out.printf("The sample standard deviation of the Double test set = %.2f\n", standardDeviation(testDataD));
+
     }
+*/
 }
